ef46fcd3ace930e4fb3d94d3a664c6957f861bec
ryandavidhartman <ryandavidhartman@gmail.com>
2021-07-21 20:26:00 -0400

    up to step six in the demo instructions

--- demo1/build.sbt
+++ demo1/build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.13.1" // or any other Scala version >= 2.10.2
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "1.1.0"
--- demo1/src/main/scala/tutorial/webapp/TutorialApp.scala
+++ demo1/src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,8 +1,34 @@
 package tutorial.webapp
 
+import org.scalajs.dom
+import org.scalajs.dom.document
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
+      setupUI()
+    })
+  }
+
+  def setupUI(): Unit = {
+    appendPar(document.body, "Hello World")
+
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
+  }
+
+  //@JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
