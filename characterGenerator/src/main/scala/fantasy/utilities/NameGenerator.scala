cca1f62305c16f886b0a7bde6dba8cce73d20b79
ryandavidhartman <ryandavidhartman@gmail.com>
2022-06-03 23:46:21 -0400

    added human female names

--- characterGenerator/src/main/scala/fantasy/utilities/NameGenerator.scala
+++ characterGenerator/src/main/scala/fantasy/utilities/NameGenerator.scala
@@ -5,15 +5,15 @@ object NameGenerator {
   def getName(race: String, gender: String): String = race match {
     case "Human" =>
       if(gender == "Male")
-        s"$getHumanMaleName $getHumanSurname"
+        s"$getHumanMaleFirstName $getHumanSurname"
       else
-        "bob"
+        s"$getHumanFemaleFirstName $getHumanSurname"
     case _ => "bob"
   }
-  
-  
+
+
   def getHumanSurname(): String = {
-    
+
     val surNames: Seq[String] = Seq("Hill", "Morton", "Hutchinson", "Hutchinson", "Brooks", "Montgomery", "Jensen",
       "Rogers", "Mccarthy", "O'Quinn", "Gardner", "Booth", "Pham", "Austin", "Jackson", "Day", "Ayala", "Page",
       "Vazquez", "Zang", "Rice", "Wolf", "Abbott", "Mcguire", "Aguilar", "Mckinney", "Henry","Baker", "Ross", "Lowe",
@@ -33,12 +33,12 @@ object NameGenerator {
       "Terry", "Leonard", "Allen", "Barnett", "Herbert", "Boyd", "Naylor", "Sims", "Joyce", "Sharp", "Patterson",
       "Nichols", "Thompson", "Moran", "Figueroa", "Graham", "Fuller", "Reed", "Horton", "Kaye", "Reynolds", "Rossi",
       "Nicholson", "Brady", "Black", "Gallegos")
-    
+
     val count = surNames.length
     surNames(Roller.randomInt(count))
   }
-  
-  def getHumanMaleName(): String = {
+
+  def getHumanMaleFirstName(): String = {
 
     val firstName: Seq[String] = Seq("Arkandor", "Aeryk", "Aubir", "Birker", "Coro", "Charn", "Dunni", "Dhenor",
       "Enlo", "Ferdirk", "Golian", "Ghörn", "Hold", "Ivor", "Jarum", "Kolm", "Kherano", "Laef", "Mannur", "Nixor",
@@ -49,6 +49,24 @@ object NameGenerator {
       "Ulrekk", "Vigor", "Thorold", "Wulfer","Xeron", "Yarlo", "Zorn", "Ilfarulv", "Jonnus", "Merlot", "Mimur",
       "Neolo", "Borker", "Oerkur", "Parus", "Pertus", "Rialter", "Raulfur", "Thul", "Tirul", "Timbul", "Sturv", "Särkor",
       "Sturmnir", "Solan", "Svert", "Ernoldus", "Albinus",  "Ailmar",     "Gredlan" ,"Gaufridus", "Turbert")
+
+    val count = firstName.length
+    firstName(Roller.randomInt(count))
+  }
+
+  def getHumanFemaleFirstName(): String = {
+
+    val firstName: Seq[String] = Seq("Alonina", "Aena", "Aula", "Belea", "Celinda", "Chistri", "Dorah", "Dhunha",
+      "Essai", "Flora", "Gunn", "Gharee", "Hejlaga", "Ingera", "Jess", "Kolmee", "Khira", "Lomma", "Moniské",
+      "Nonna", "O'sarena", "Paladina", "Phera", "Quenn", "Reena", "Sunni", "Shaelah", "Skalda", "Tulla", "Thera",
+      "Urvella", "Verona", "Wienna", "Xorone", "Ylfmar", "Zara", "Åsha", "Älta", "Öja", "Annalyn", "Bridonna",
+      "Celestine", "Chasnera", "Desini", "Efari", "Fritha", "Gwenna", "Halette", "Ilenne", "Justaline", "Kassathra",
+      "Khirta", "Lonna", "Maritt", "Nixi", "Orana", "Polona", "Phora", "Qannae", "Rohana", "Reya", "Ritta", "Selena",
+      "Stina", "Stella", "Tressa", "Uneste", "Ulrekkana", "Vehlma", "Verkaa", "Wolkaee", "Xera", "Yenna", "Zelma",
+      "Amara", "Ada", "Ani", "Aelli", "Alandra", "Belga", "Alba", "Branda", "Beasir", "Chelena", "Delia", "Dorah",
+      "Debera", "Ellena", "Eiri", "Halaë", "Ilda", "Mirla", "Tembla", "Sinea", "Senessa", "Salana", "Sonna", "Stella",
+      "Alta",
+    )
 
     val count = firstName.length
     firstName(Roller.randomInt(count))
