dcf8e7635c923d2e2e312a7705fd2d838fa53ea1
ryandavidhartman <ryandavidhartman@gmail.com>
2022-06-28 22:30:33 -0400

    working on the new background generator

--- characterGenerator/src/main/scala/fantasy/utilities/BackgroundGenerator2.scala
+++ characterGenerator/src/main/scala/fantasy/utilities/BackgroundGenerator2.scala
@@ -2,6 +2,11 @@ package fantasy.utilities
 
 object BackgroundGenerator2 {
 
+  def getRandomData(data: Map[Int, String]): String = {
+    val random = Roller.randomInt(data.size)+1
+    data(random)
+  }
+
   def getBirthOrder(): String = {
     val birthOrders: Map[Int, String] = Map(
       1 -> "First born",
@@ -16,8 +21,101 @@ object BackgroundGenerator2 {
       10 -> "Eighth or more"
     )
 
-    val random = Roller.randomInt(birthOrders.size)+1
-    birthOrders(random)
+    getRandomData(birthOrders)
+  }
+
+  def getCrafts(): String = {
+    val crafts: Map[Int, String] = Map(
+      1 -> "Tailor",
+      2 -> "Fletcher/Bowyer",
+      3  -> "Glass Blower",
+      4  -> "Carpenter",
+      5  -> "Animal trainer/Beast master",
+      6  -> "Cartographer",
+      7  -> "Smith",
+      8  -> "Cobbler",
+      9  -> "Weaver",
+      10  -> "Armorer",
+      11  -> "Brewer",
+      12  -> "Mason",
+      13  -> "Potter",
+      14  -> "Miller",
+      15  -> "Dyer",
+      16  -> "Shipwright",
+      17  -> "Jeweler",
+      18  -> "Artist",
+      19  -> "Sculptor",
+      20  -> "Musician",
+      21  -> "Baker",
+      22  -> "Weaponsmith",
+    )
+    getRandomData(crafts)
+  }
+
+  def getGovernmentJob(): String = {
+    val governmentJobs = Map(
+      1 -> "Tax collector",
+      2 -> "Tex collector",
+      3 -> "Sheriff",
+      4 -> "Forest warden",
+      5 -> "Magistrate",
+      6 -> "Town mayor",
+      7 -> "City mayor",
+      8 -> "Ducal advisor",
+      9 -> "Royal advisor",
+    )
+    getRandomData(governmentJobs)
+  }
+
+  def getMerchantJob(): String = {
+    val merchantJobs = Map(
+      1 -> "Shopkeeper, foodstuffs",
+      2 -> "Shopkeeper, dry goods",
+      3 -> "Shopkeeper, exotic goods",
+      4 -> "Innkeeper",
+      5 -> "Local trader",
+      6 -> "Caravan trader"
+    )
+    getRandomData(merchantJobs)
+  }
+
+  def getClergyJob(): String = {
+    val clergyJobs = Map(
+      1 -> "Parish Priest (mainstream religion)",
+      2 -> "Parish Priest (mainstream religion)",
+      3 -> "Upper clergy (mainstream religion)",
+      4 -> "Clergy (heretic religion)",
+      5 -> "Druid",
+      6 -> "Priest of Elder gods"
+    )
+    getRandomData(clergyJobs)
+  }
+
+  def getNobility(): String = {
+    val nobility = Map(
+      1 -> "Landless Knight",
+      2 -> "Landless Knight",
+      3 -> "Landless Knight",
+      4 -> "Knight Banneret",
+      5 -> "Knight Banneret",
+      6 -> "Knight",
+      7 -> "Knight",
+      8 -> "Knight",
+      9 -> "Knight",
+      10 -> "Knight",
+      11 -> "Earl",
+      12 -> "Earl",
+      13 -> "Baron",
+      14 -> "Baron",
+      15 -> "Baron",
+      16 -> "Count",
+      17 -> "Marquis",
+      18 -> "Duke",
+      19 -> "Arch Duke",
+      20 -> "Prince",
+      21 -> "King"
+    )
+    getRandomData(nobility)
   }
 
   def getParentsOccupation: String = {
@@ -48,112 +146,62 @@ object BackgroundGenerator2 {
         23 -> "Genteel",
         24 -> getNobility()
       )
-      val random = Roller.randomInt(occupations.size)+1
-      occupations(random)
+    getRandomData(occupations)
     }
 
-
-  def getCrafts(): String = {
-    val crafts: Map[Int, String] = Map(
-      1 -> "Tailor",
-      2 -> "Fletcher/Bowyer",
-      3  -> "Glass Blower",
-      4  -> "Carpenter",
-      5  -> "Animal trainer/Beast master",
-      6  -> "Cartographer",
-      7  -> "Smith",
-      8  -> "Cobbler",
-      9  -> "Weaver",
-      10  -> "Armorer",
-      11  -> "Brewer",
-      12  -> "Mason",
-      13  -> "Potter",
-      14  -> "Miller",
-      15  -> "Dyer",
-      16  -> "Shipwright",
-      17  -> "Jeweler",
-      18  -> "Artist",
-      19  -> "Sculptor",
-      20  -> "Musician",
-      21  -> "Baker",
-      22  -> "Weaponsmith",
+  def getRelative(): String = {
+    val relatives = Map(
+      1 -> "Brother/Sister",
+      2 -> "First Cousin",
+      3 -> "Uncle/Aunt",
+      4 -> "Grandfather/mother",
+      5 -> "Great Uncle/aunt",
+      6 -> "Distant relation"
     )
-    val random = Roller.randomInt(crafts.size)+1
-    crafts(random)
+
+    getRandomData(relatives)
   }
 
-  def getGovernmentJob(): String = {
-    val governmentJobs = Map(
-      1 -> "Tax collector",
-      2 -> "Tex collector",
-      3 -> "Sheriff",
-      4 -> "Forest warden",
-      5 -> "Magistrate",
-      6 -> "Town mayor",
-      7 -> "City mayor",
-      8 -> "Ducal advisor",
-      9 -> "Royal advisor",
+  def getGuardians(): String = {
+    val guardians: Map[Int, String] = Map(
+      1 -> "Wicked/cruel stepmother or father",
+      2 -> "Hedge Wizard/Mage",
+      3 -> "Monastery/Convent",
+      4 -> getCrafts(),
+      5 -> getRelative(),
+      6 -> "Sold into slavery",
+      7 -> "Raised by wolves",
+      8 -> "Adventurer",
+      9 -> "Dwarven indentured servants",
+      10 -> "Mysterious red-robed /“elven/” guardians",
+      11 -> "Centaurs/Hobgoblins/Deep Ones/ other monsters",
+      12 -> "Raised by mercenaries",
+      13 -> "Bandits/pirates",
+      14 -> "Nomads/cossacks/barbarians",
+      15 -> s"Adopted by ${getMerchantJob()}",
+      16 -> s"Adopted by ${getClergyJob()}",
+      17 -> s"Adopted by a ${getNobility()}",
+      18 -> "Lived on the streets/no guardian",
+      19 -> "Lived on the streets/no guardian",
+      20 -> "Lived on the streets/no guardian",
     )
-
-    val random = Roller.randomInt(governmentJobs.size)+1
-    governmentJobs(random)
+    getRandomData(guardians)
   }
 
-  def getMerchantJob(): String = {
-    val merchantJobs = Map(
-      1 -> "Shopkeeper, foodstuffs",
-      2 -> "Shopkeeper, dry goods",
-      3 -> "Shopkeeper, exotic goods",
-      4 -> "Innkeeper",
-      5 -> "Local trader",
-      6 -> "caravan trader"
+  def getLifeEvents: String = {
+    val events: Map[Int, String] = Map(
+      1 -> "Loved/protected by parents",
+      2 -> "Unloved/spurned by parents",
+      3 -> getGuardians(), //Adopted
+      4 -> "Peasant/Farm worker", //Family killed by Other
+      5 -> "Farmer",
+      6 -> "Fisherman",
+      7 -> "Miner",
+      8 -> "Forester",
+      9 -> "Sailor",
+      10 -> "Soldier",
     )
-
-    val random = Roller.randomInt(merchantJobs.size)+1
-    merchantJobs(random)
-  }
-
-  def getClergyJob(): String = {
-    val clergyJobs = Map(
-      1 -> "Parish Priest (mainstream religion)",
-      2 -> "Parish Priest (mainstream religion)",
-      3 -> "Upper clergy (mainstream religion)",
-      4 -> "Clergy (heretic religion)",
-      5 -> "Druid",
-      6 -> "Priest of Elder gods"
-    )
-
-    val random = Roller.randomInt(clergyJobs.size)+1
-    clergyJobs(random)
-  }
-
-  def getNobility(): String = {
-    val nobility = Map(
-      1 -> "Landless Knight",
-      2 -> "Landless Knight",
-      3 -> "Landless Knight",
-      4 -> "Knight Banneret",
-      5 -> "Knight Banneret",
-      6 -> "Knight",
-      7 -> "Knight",
-      8 -> "Knight",
-      9 -> "Knight",
-      10 -> "Knight",
-      11 -> "Earl",
-      12 -> "Earl",
-      13 -> "Baron",
-      14 -> "Baron",
-      15 -> "Baron",
-      16 -> "Count",
-      17 -> "Marquis",
-      18 -> "Duke",
-      19 -> "Arch Duke",
-      20 -> "Prince",
-      21 -> "King"
-    )
-
-    val random = Roller.randomInt(nobility.size)+1
-    nobility(random)
+    getRandomData(events)
   }
 
 }
