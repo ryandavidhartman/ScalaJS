8d501235e534d7bbd9768444b8a6910885769229
ryandavidhartman <ryandavidhartman@gmail.com>
2022-09-09 22:50:58 -0400

    I thought I saved this already

--- characterGenerator/src/main/scala/basic/fantasy/characterclass/CharacterState.scala
+++ characterGenerator/src/main/scala/basic/fantasy/characterclass/CharacterState.scala
@@ -1,3 +1,26 @@
 package basic.fantasy.characterclass
 
-case class CharacterState()
+import basic.fantasy.backgrounds.CharacterAlignment
+import basic.fantasy.backgrounds.Races.Race
+import basic.fantasy.characterclass.CharacterClasses.CharacterClass
+
+case class CharacterState(
+   name: String,
+   race: Race,
+   characterClass: CharacterClass,
+   level: Int,
+   gender: String,
+   height: String,
+   weight: Int,
+   age: Int,
+   alignment: CharacterAlignment,
+   personality: String,
+   strength: Int,
+   dexterity: Int,
+   constitution: Int,
+   intelligence: Int,
+   wisdom: Int,
+   charisma: Int,
+   attackBonus: String,
+
+)
